261afc37f9aa27579d74db1e85c746647bb20f3d
Sahil-B07 <objectorientedguy23@gmail.com>
2022-07-07 23:18:06 +0530

    add-ons

--- main.py
+++ main.py
@@ -1,27 +1,80 @@
-import telebot
-import os
+import telebot 
+import os, requests
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 API_KEY = os.getenv('Otaku_API')
 
 bot = telebot.TeleBot(API_KEY)
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+def Imarkup():
+	markup = InlineKeyboardMarkup()
+	markup.row_width = 2
+	markup.add(
+		InlineKeyboardButton("🔎 Music", callback_data="music"),
+		InlineKeyboardButton("🔎 Gifs", callback_data="gify"),
+		InlineKeyboardButton("🔎 Search gif inline", callback_data="inlineGif")
+	)
+	return markup 
+
+# Call back actions
+@bot.callback_query_handler(func=lambda message: True)
+def callback_query(call):
+	if call.data == "music":
+		bot.answer_callback_query(call.id, "")
+		songs(call)
+	elif call.data == "gify":
+		bot.answer_callback_query(call.id, "")
+		giffy(call)
+	elif call.data == "inlineGif":
+		bot.answer_callback_query(call.id, "Convention!")
+		bot.send_message(call.from_user.id,"use /gif then type your context\nExample:\n1./gif anime\n2./gif smile\n...")
 
 
-gifFile = "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/5eeea355389655.59822ff824b72.gif"
-audioFile = "https://mp3gaga.com/wp-content/uploads/2021/03/COOLIO-GANGSTER-PARADISE-Mp3gaga.com_.mp3"
+# Commands
 
+@bot.message_handler(commands=['start', 'help', 'music'])
+def commands(message):
+	userName = message.from_user.username
 
-@bot.message_handler(commands=['music','gif'])
-def music_on(message):
-	if message.text == "/gif":
-		bot.send_document(message.chat.id,gifFile)
+	if message.text == "/start":
+		bot.reply_to(message, f"{userName}, how are you doing?", reply_markup=Imarkup())
+	elif message.text == "/help":
+		bot.reply_to(message, "/gif - get some intresting gifs\n/music - Listen to your fav music\ninline GIF - use /gif then type your context")
 	elif message.text == "/music":
 		# bot.send_audio("@otaku_testing", audioFile)  # send the file to the channel
-		bot.send_audio(message.chat.id, audioFile)
-		
+		# bot.send_audio(message.from_user.id, audioFile)
+		songs(message)
+
+@bot.message_handler(func=lambda message: True)
+def inlineGif(message):
+	inpText = message.text
+	if "/gif" in inpText:
+		context = inpText.replace("/gif", " ").strip()
+		if context != "":
+			giffy(message, context)
+		else:
+			giffy(message)
+	else:
+		bot.send_message(message.from_user.id,"Wrong Input\nTry again!", reply_markup=Imarkup())
+
+
+
+#Tenor
+apikey = "LIVDSRZULELA" 
+lmt = 1
+
+# Functions 
+
+audioFile = "https://mp3gaga.com/wp-content/uploads/2021/03/COOLIO-GANGSTER-PARADISE-Mp3gaga.com_.mp3"
+
+def giffy(message, search_term = "smily"):
+	r = requests.get("https://g.tenor.com/v1/random?&q=%s&key=%s&limit=%s" % (search_term,apikey,lmt)).json()
+	imageUrl = str(r['results'][0]['url'])
+	bot.send_document(message.from_user.id,imageUrl)
+
+def songs(message):
+	# bot.send_audio("@otaku_testing", audioFile)  # send the file to the channel
+	bot.send_audio(message.from_user.id, audioFile)
 
 
 bot.infinity_polling()
