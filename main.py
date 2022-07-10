78408f1014390e743df4d292a944506b45f9de14
Sahil-B07 <objectorientedguy23@gmail.com>
2022-07-10 15:18:02 +0530

    updates

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ def Imarkup():
 def callback_query(call):
 	if call.data == "music":
 		bot.answer_callback_query(call.id, "")
-		songs(call)
+		songs(call.message)
 	elif call.data == "gify":
 		bot.answer_callback_query(call.id, "")
-		giffy(call)
+		giffy(call.message)
 	elif call.data == "inlineGif":
 		bot.answer_callback_query(call.id, "Convention!")
 		bot.send_message(call.from_user.id,"use /gif then type your context\nExample:\n1./gif anime\n2./gif smile\n...")
@@ -36,31 +36,32 @@ def callback_query(call):
 @bot.message_handler(commands=['start', 'help', 'music','igif'])
 def commands(message):
 	userName = message.from_user.username
-
 	if message.text == "/start":
 		bot.reply_to(message, f"{userName}, how are you doing?", reply_markup=Imarkup())
 	elif message.text == "/help":
 		bot.reply_to(message, "/gif - get some intresting gifs\n/music - Listen to your fav music\ninline GIF - use /gif then type your context")
 	elif message.text == "/music":
-		# bot.send_audio("@otaku_testing", audioFile)  # send the file to the channel
+		bot.send_audio("@otaku_testing", audioFile)  # send the file to the channel
 		# bot.send_audio(message.from_user.id, audioFile)
 		songs(message)
 	elif message.text == "/igif":
-		bot.send_message(message.from_user.id,"use /gif then type your context\nExample:\n1./gif anime\n2./gif smile\n...")
+		bot.send_message(message.chat.id,"use /gif then type your context\nExample:\n1./gif anime\n2./gif smile\n...")
 
-
+# inline commands
 
 @bot.message_handler(func=lambda message: True)
 def inlineGif(message):
 	inpText = message.text
 	if "/gif" in inpText:
 		context = inpText.replace("/gif", " ").strip()
+		botMsg = bot.reply_to(message,"Searching Gif...")
 		if context != "":
 			giffy(message, context)
 		else:
 			giffy(message)
+		bot.delete_message(message.chat.id, botMsg.message_id)
 	else:
-		bot.send_message(message.from_user.id,"Wrong Input\nTry again!", reply_markup=Imarkup())
+		bot.send_message(message.chat.id,"Wrong Input\nTry again!", reply_markup=Imarkup())
 
 command = [BotCommand("start","to start the bot"), BotCommand("help","A guide to use Otaku"),
 		    BotCommand("gif","Get random gifs"), BotCommand("igif","Inline gif search"),
@@ -68,23 +69,21 @@ command = [BotCommand("start","to start the bot"), BotCommand("help","A guide to
 bot.set_my_commands(command)
 
 #Tenor
+
 apikey = "LIVDSRZULELA" 
 lmt = 1
 
 # Functions 
 
 audioFile = "https://mp3gaga.com/wp-content/uploads/2021/03/COOLIO-GANGSTER-PARADISE-Mp3gaga.com_.mp3"
-
 def giffy(message, search_term = "Hello"):
 	r = requests.get("https://g.tenor.com/v1/random?&q=%s&key=%s&limit=%s" % (search_term,apikey,lmt)).json()
 	imageUrl = str(r['results'][0]['url'])
-	botMsg = bot.reply_to(message,"Searching Gif...")
-	bot.send_document(message.from_user.id,imageUrl)
-	bot.delete_message(message.from_user.id, botMsg.message_id)
-
+	bot.send_document(message.chat.id,imageUrl)
+	
 def songs(message):
 	# bot.send_audio("@otaku_testing", audioFile)  # send the file to the channel
-	bot.send_audio(message.from_user.id, audioFile)
+	bot.send_audio(message.chat.id, audioFile)
 
 
 bot.infinity_polling()
