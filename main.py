7e7d0a3ace1a610bd77cc844c3cdd75e9ed10798
Sahil-B07 <objectorientedguy23@gmail.com>
2022-07-13 15:05:41 +0530

    update

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def songs(message):
 	path = './Downloads/'
 
 	with open(path + 'jjk.mkv', 'wb') as file:
-		response = requests.get('https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/5eeea355389655.59822ff824b72.gif')
+		response = requests.get('https://dl.animesp.xyz/Completed/Jujutsu%20Kaisen/720/Jujutsu%20Kaisen%20-%2001%20%5B720p%20x265%20BD%5D.mkv')
 		file.write(response.content)
 
 		giFile = path + os.listdir(path)[0]
